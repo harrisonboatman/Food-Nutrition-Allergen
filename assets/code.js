11c714b507261b687d7e503bea7c1d33ee3bd652
Harrison Boatman <hrry.boatman2@gmail.com>
2023-01-12 13:48:47 -0600

    Updated JS functionality to include fetching first api

--- assets/code.js
+++ assets/code.js
@@ -1,4 +1,11 @@
 var submitBtn = document.querySelector("#submitBtn")
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'f22fe07855mshbcbd682b9e3da6ap1cac5ajsn940b652f81a9',
+		'X-RapidAPI-Host': 'recipe-by-api-ninjas.p.rapidapi.com'
+	}
+};
 
 function handleFormSubmit () {
     var searchEl = document.querySelector('#search-input').value; 
@@ -6,25 +13,35 @@ function handleFormSubmit () {
     console.log(searchEl);
     console.log(allergenEl);
 
-    getRecipes(searchEl, allergenEl);
+    getRecipes(searchEl);
 }
 
-function getRecipes (recipe, allergen);
-var repiceURL = ""
+function getRecipes (recipe) {
+var recipeURL = "https://recipe-by-api-ninjas.p.rapidapi.com/v1/recipe?query=" + recipe + "&offset=10"
+console.log(recipeURL)
 
-    fetch(recipeURL)
+    fetch(recipeURL, options)
     .then(function(response){
         if (!response.ok) {
             console.error("you messed up");
             return
         } else {
             console.log(response);
-            response.json();
+            return response.json();
         }
     })
     .then(function(recResult){
         console.log(recResult);
-
+        console.log(recResult[0].ingredients)
     })
 
+    checkAllergen();
+}
+
+function checkAllergen(){
+    var allergenEl = document.querySelector('#allergen-input').value;
+    console.log(allergenEl);
+
+}
 submitBtn.addEventListener('click', handleFormSubmit);
+
