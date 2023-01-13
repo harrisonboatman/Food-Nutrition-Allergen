f642497f8c28681090ee1d8b3c65434a70e0b3af
Harrison Boatman <hrry.boatman2@gmail.com>
2023-01-13 13:44:29 -0600

    inputs recipe names and instructions into the cards

--- assets/code.js
+++ assets/code.js
@@ -9,6 +9,11 @@ var card2 = document.querySelector("#card2");
 var card3 = document.querySelector("#card3");
 var card4 = document.querySelector("#card4");
 var card5 = document.querySelector("#card5");
+var instruct1 = document.querySelector('#instruct1');
+var instruct2 = document.querySelector('#instruct2');
+var instruct3 = document.querySelector('#instruct3');
+var instruct4 = document.querySelector('#instruct4');
+var instruct5 = document.querySelector('#instruct5');
 const options = {
     method: 'GET',
     headers: {
@@ -30,6 +35,7 @@ function getRecipes(recipe) {
     var recipeURL = "https://recipe-by-api-ninjas.p.rapidapi.com/v1/recipe?query=" + recipe + "&offset=4"
     var allergenEl = document.querySelector('#allergen').value;
     var cardArr = [card1, card2, card3, card4, card5]
+    var instructArr = [instruct1, instruct2, instruct3, instruct4, instruct5];
     var allergyAlert = document.querySelector('#card')
     console.log(recipeURL)
 
@@ -50,17 +56,22 @@ function getRecipes(recipe) {
             console.log(ingredArray);
             console.log(ingredArray.length)
             for(i=0; i<cardArr.length; i++) {
-                cardArr[i].textContent;
+                cardArr[i].textContent = recResult[i].title;
+                instructArr[i].textContent = recResult[i].instructions;
+
+            
+            //     for (i=0; i<ingredArray.length; i++ ){
+            //     console.log(ingredArray[i].includes(allergenEl))
+            //     if (ingredArray[i].includes(allergenEl)){
+            //         allergyAlert.classList.add("red-text")
+            //         break;
+            //     }
+            // }
+            
             }
 
 
-            for (i=0; i<ingredArray.length; i++ ){
-                console.log(ingredArray[i].includes(allergenEl))
-                if (ingredArray[i].includes(allergenEl)){
-                    allergyAlert.classList.add("red-text")
-                    break;
-                }
-            }
+            
         })
 
     checkAllergen();
